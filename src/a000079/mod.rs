bd90393fc7236a9c65226b972180e8d87741fa07
kitsuyui <kitsuyui+github@kitsuyui.com>
2026-06-04 23:24:49 +0900

    Restrict a000079 to unsigned types via num_traits::Unsigned bound

--- src/a000079/mod.rs
+++ src/a000079/mod.rs
@@ -3,7 +3,7 @@
 /// Power of 2
 pub fn a000079<T>(n: T) -> T
 where
-    T: std::ops::Shl<Output = T> + From<u8>,
+    T: std::ops::Shl<Output = T> + From<u8> + num_traits::Unsigned,
 {
     T::from(1) << n
 }
@@ -15,7 +15,7 @@ mod tests {
 
     #[test]
     fn test_a000079() {
-        let expected = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024];
+        let expected: [u32; 11] = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024];
         assert_sequence(a000079, &expected);
     }
 }
